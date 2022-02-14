4d9a64b9a288935b7c050444add9ae93594fa748
sabariganesan <sabariganesan01@gmail.com>
2022-02-14 14:23:38 +0530

    initial comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Content from './component/content/Content'
+import Head from './component/header/Header'
+
+function App() {
+  return (
+    <div>
+      <Head/>
+      <Content/>
+    </div>
+  )
+}
+
+export default App
